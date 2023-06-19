fd1861442e1bf2d2d0126c03d3271b9c652c93bc
Achinta Mistry <achintachu@gmail.com>
2023-06-19 20:02:17 +0000

    unwanted changes removed

--- src/app.py
+++ src/app.py
@@ -1,10 +1,8 @@
-"""Flask Application"""
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 
 # load modules
 from src.routes.api import youtube_api
-from src.routes.swagger import swagger_ui_blueprint, SWAGGER_URL
+from werkzeug.exceptions import HTTPException
 
 # init Flask app
 app = Flask(__name__)
@@ -12,28 +10,19 @@ app = Flask(__name__)
 # register blueprints. ensure that all paths are versioned!
 app.register_blueprint(youtube_api, url_prefix="/youtube-seo-optimize")
 
-from src.api_spec import spec
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "error": {
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        }
+    })
+    response.content_type = "application/json"
+    return response
 
-
-@app.route("/api/swagger.json")
-def create_swagger_spec():
-    """
-    Swagger API definition.
-    """
-    return jsonify(spec.to_dict())
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return jsonify(error=404, text=str(e)), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return jsonify(error=500, text=str(e)), 500
-
-
-app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
